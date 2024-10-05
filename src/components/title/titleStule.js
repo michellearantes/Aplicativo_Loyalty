cef2d098e90c0f1b751d56a263da7a0869945645
michellearantes <michelle.arantes@gmail.com>
2024-10-05 11:47:03 -0300

    Primeiro commit

---
+++ src/components/title/titleStule.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    boxTitle:{
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 5,
+        width: "100%",
+ },
+    TextTitle:{
+        color: "#cedbfa",
+        fontSize: 28,
+        fontWeight: "bold",
+        marginTop: 10,
+        textAlign: "center"
+    },
+    campTitle:{
+        width: "100%"
+    }
+
+})
+
+export default styles
